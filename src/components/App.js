9095072402439a8a992cca4b83d0bf24039a2f30
LAPTOP-RLCTEB5O\wangek <wangerk@163.com>
2023-05-03 07:10:40 +0800

    1222

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <div>
       <p>
         {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
-        <strong>{results.length}</strong> 我是飞猪
+        <strong>{results.length}</strong> 飞猪来啦
       </p>
       <hr></hr>
       {results
